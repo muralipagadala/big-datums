e56e02a57ca0071d729ed5c14c1d3192f1793a96
nik <nronntag@gmail.com>
2015-12-09 21:06:47 -0800

    renaming

---
+++ programming/java/big-datums/src/test/java/com/bigdatums/aws/BdS3ClientTest.java
@@ -0,0 +1,38 @@
+package com.bigdatums.aws;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+import java.io.IOException;
+import java.util.List;
+
+public class BdS3ClientTest {
+
+    @Test
+    public void testS3FileCount() throws IOException{
+
+        //BdS3Client client
+        BdS3Client s3Client = new BdS3Client();
+
+        //create bucket for test
+        String bucketName = "s3-count-test";
+        s3Client.createBucket(bucketName);
+
+        //push empty files to bucket
+        int filesToPush = 10;
+        s3Client.putEmptyFiles(bucketName, "BdS3ClientTest-", "txt", filesToPush);
+
+        //get number of items in bucket
+        List<String> bucketKeys = s3Client.getBucketObjectNames(bucketName);
+        int numOfObjects = bucketKeys.size();
+
+        //test list contains all objects in bucket
+        Assert.assertEquals(numOfObjects, filesToPush);
+
+        //delete temp objects and bucket
+        s3Client.deleteAllFromBucket(bucketName);
+        s3Client.deleteBucket(bucketName);
+
+    }
+
+}
