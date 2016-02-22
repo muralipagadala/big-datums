e6012f8f810d28cef100eb8420122050b1b02d98
nik <nronntag@gmail.com>
2016-02-22 00:29:37 -0800

    adding get nth line in linux example

---
+++ linux-shell/get_nth_line_of_file.sh
@@ -0,0 +1,15 @@
+#read 25th line of sample_data_1.txt
+
+#with head/tail
+cat sample_data_1.txt | head -25 | tail -1
+
+#with sed p/print
+cat sample_data_1.txt | sed -n '25p'
+
+#with sed d/delete
+cat sample_data_1.txt | sed '25!d'
+
+#3 variations with awk "NR"
+cat sample_data_1.txt | awk 'NR==25'
+cat sample_data_1.txt | awk 'NR==25{print}'
+cat sample_data_1.txt | awk '{if(NR==25) print}'
