c9beafc9d23e87ba08a26a4a523184e776613d41
nsonntag <nronntag@gmail.com>
2016-09-10 23:32:05 -0700

    adding loop over dates example

---
+++ linux-shell/loop-over-dates.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#set start and end dates in specified format
+start_dt=`date +"%Y-%m-%d" -d "2020-01-01"`
+end_dt=`date +"%Y-%m-%d" -d "2021-01-01"`
+
+#use $current_dt in loop
+current_dt=$start_dt
+
+#loop over dates in range and print
+while [ $current_dt != $end_dt ];
+do 
+  echo $current_dt;
+  
+  #add 1 day to current_dt
+  current_dt=`date +"%Y-%m-%d" -d "$current_dt + 1 day"`;
+done
